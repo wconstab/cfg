e397f8d4b980fb68c6c6caa85c5e5e44064878ba
Will Constable <will@nervanasys.com>
2016-04-17 05:36:34 +0000

    fixes, tmux ssh agent working now

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 for FILE in .vimrc .tmux.conf .ssh/rc;
 do
-    if [ -a $FILE ]; then
+    if [ -a ~/$FILE ]; then
         echo "Skipping $FILE"
     else
-        ln -s $FILE ~/$FILE
+        FILE_PATH=`pwd $FILE`
+        ln -s $FILE_PATH/$FILE ~/$FILE
     fi
 done
